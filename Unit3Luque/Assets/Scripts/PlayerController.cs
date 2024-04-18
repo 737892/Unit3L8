b7dbaff19d947e317c5534aa15578466d6cd781b
Alejandra L. <737892@student.dusd.net>
2024-04-18 10:00:15 -0700

    adde4d sound effectd

--- Unit3Luque/Assets/Scripts/PlayerController.cs
+++ Unit3Luque/Assets/Scripts/PlayerController.cs
@@ -10,21 +10,32 @@ public class PlayerController : MonoBehaviour
     public float gravityModifier;
     public bool isOnGround = true;
     public bool gameOver = false;
+    private Animator playerAnim;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    private AudioSource playerAudio;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
+        playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
         
         }
 
@@ -33,9 +44,25 @@ public class PlayerController : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
+        {
             isOnGround = true;
+            dirtParticle.Play();
+        }
+            
         else if (collision.gameObject.CompareTag("Obstacle"))
+        
+      
+        {
             gameOver = true;
-        Debug.Log("Game Over!");
+            Debug.Log("Game Over!");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
+            dirtParticle.Stop();
+            {
+                playerAudio.PlayOneShot(crashSound, 1.0f);
+            }
+        }
+            
     }
 }
